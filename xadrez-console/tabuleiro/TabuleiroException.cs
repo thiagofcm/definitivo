e28ad496fe98c5a3269f4559f19c3ad12bf90045
thiagofcm <thiagof1503@gmail.com>
2016-10-20 23:13:52 -0200

    Tratando erros com exceçao personalizada

--- xadrez-console/tabuleiro/TabuleiroException.cs
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace xadrez_console.tabuleiro
-{
-    class TabuleiroException
+namespace tabuleiro{
+    class TabuleiroException :  Exception
     {
+    public TabuleiroException (string msg) : base (msg){
+
+    }
     }
 }
